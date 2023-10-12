70e5f3946d4f088d6f3ae9829326092305c97866
unknown <tobiiduarte95@gmail.com>
2023-10-12 15:54:11 -0300

    store with redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import { Store } from "./store/Store.js";
+import { Store, persistor } from "./store/Store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={Store}>
-    <App />
-  </Provider>
+	<>
+		<Provider store={Store}>
+			<PersistGate persistor={persistor}>
+				<App />
+			</PersistGate>
+		</Provider>
+	</>
 );
--- src/store/Store.js
+++ src/store/Store.js
@@ -1,5 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+
+const reducers = combineReducers({});
+
+export const persistConfig = {
+	key: "root",
+	storage,
+	whileList: [],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const Store = configureStore({
-  reducer: {},
+	reducer: persistedReducer,
 });
+
+export const persistor = persistStore(Store);
