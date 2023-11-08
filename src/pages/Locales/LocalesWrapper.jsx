442db31e411965f0fa9aec8393cca358e99dbc09
Tobias Duarte <tobiiduarte95@gmail.com>
2023-11-08 19:01:34 -0300

    responsivee

--- src/pages/Locales/LocalesWrapper.jsx
+++ src/pages/Locales/LocalesWrapper.jsx
@@ -1,27 +1,27 @@
 import {
-	InfoTittle,
-	LocalWrapper,
-	LocalesSection,
+  InfoTittle,
+  LocalWrapper,
+  LocalesSection,
 } from "./LocalesWrapperStyled";
 
 import { AllLocales } from "../../data/DataAllLocales";
 import CardLocales from "../../components/CardLocalesWrapper/CardLocales";
 
 const LocalesWrapper = () => {
-	return (
-		<>
-			<LocalesSection>
-				<InfoTittle>
-					Los mejores locales de viandas en un solo lugar 😋
-				</InfoTittle>
-				<LocalWrapper>
-					{AllLocales.map((local) => {
-						return <CardLocales key={local.id} {...local} />;
-					})}
-				</LocalWrapper>
-			</LocalesSection>
-		</>
-	);
+  return (
+    <>
+      <LocalesSection>
+        <InfoTittle>
+          Los mejores locales de viandas en un solo lugar 😋
+        </InfoTittle>
+        <LocalWrapper>
+          {AllLocales.map((local) => {
+            return <CardLocales key={local.id} {...local} />;
+          })}
+        </LocalWrapper>
+      </LocalesSection>
+    </>
+  );
 };
 
 export default LocalesWrapper;
