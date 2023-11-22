58cb7221c1fa440a044ba313c11e7f744d05ec27
unknown <tobiiduarte95@gmail.com>
2023-11-22 04:45:36 -0300

    ui localWrapper

--- src/components/CategoriesContainer/CategoriesContainerStyles.js
+++ src/components/CategoriesContainer/CategoriesContainerStyles.js
@@ -2,70 +2,70 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 export const CategoriesContainerStyled = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  max-width: 1200px;
-  padding: 10px;
-  border-radius: 15px;
-  gap: 10px;
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	max-width: 1200px;
+	padding: 10px;
+	border-radius: 15px;
+	gap: 10px;
 
-  @media (max-width: 1050px) {
-    justify-content: center;
-  }
+	@media (max-width: 1050px) {
+		justify-content: center;
+	}
 `;
 
 export const CardCategoryStyled = styled(motion.div)`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 5px;
-  text-align: center;
-  border: 1px solid white;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 5px;
+	text-align: center;
+	border: 1px solid white;
 
-  width: 150px;
-  height: 150px;
+	width: 150px;
+	height: 150px;
 
-  background-color: ${({ selected }) => (selected ? "blue" : "red")};
-  border-radius: 15px;
-  cursor: pointer;
+	background-color: ${({ selected }) => (selected ? "blue" : "red")};
+	border-radius: 15px;
+	cursor: pointer;
 
-  h2 {
-    font-size: 1rem;
-  }
+	h2 {
+		font-size: 1rem;
+	}
 
-  img {
-    width: 100px;
+	img {
+		width: 100px;
 
-    @media (max-width: 600px) {
-      width: 80px;
-    }
-  }
+		@media (max-width: 600px) {
+			width: 80px;
+		}
+	}
 
-  :first-child {
-    img {
-      padding-top: 7px;
-      padding-bottom: 7px;
-    }
-  }
+	:first-child {
+		img {
+			padding-top: 7px;
+			padding-bottom: 7px;
+		}
+	}
 `;
 
 export const PCategorias = styled.p`
-  text-align: center;
-  font-size: 1.5rem;
-  font-weight: bold;
-  color: white;
-  display: flex;
-  position: relative;
-  left: 0;
-  width: 100%;
-  border-bottom: 1px solid white;
-  padding-bottom: 5px;
-  @media (max-width: 500px) {
-    width: 50%;
-    left: 5%;
-  }
+	text-align: center;
+	font-size: 1.5rem;
+	font-weight: bold;
+	color: white;
+	display: flex;
+	position: relative;
+	align-items: center;
+	margin-top: 20px;
+	width: 100%;
+	border-bottom: 1px solid white;
+	padding-bottom: 5px;
+	@media (max-width: 500px) {
+		justify-content: center;
+	}
 `;
--- src/pages/LocalWrapper/LocalWrapperStyles.js
+++ src/pages/LocalWrapper/LocalWrapperStyles.js
@@ -1,62 +1,62 @@
 import { styled } from "styled-components";
 
 export const LocalWrapperSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  height: auto;
-  margin-top: 100px;
-  padding: 50px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: 100%;
+	height: auto;
+	margin-top: 100px;
+	padding: 50px;
 `;
 
 export const DescriptionContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  align-items: center;
-  text-align: start;
-  padding: 20px;
-  background-color: rgba(0, 0, 0, 0.5);
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	align-items: center;
+	text-align: center;
+	padding: 20px;
+	background-color: rgba(0, 0, 0, 0.5);
 `;
 
 export const TitleLocal = styled.h1`
-  font-size: 4rem;
-  font-weight: 700;
-  text-decoration: underline;
-  color: #fff;
-  text-align: center;
-  margin-bottom: 20px;
-  background: linear-gradient(
-    90deg,
-    rgba(252, 176, 69, 1) 0%,
-    rgba(253, 29, 29, 1) 50%,
-    rgba(252, 176, 69, 1) 100%
-  );
-  background-clip: text;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
+	font-size: 4rem;
+	font-weight: 700;
+	text-decoration: underline;
+	color: #fff;
+	text-align: center;
+	margin-bottom: 20px;
+	background: linear-gradient(
+		90deg,
+		rgba(252, 176, 69, 1) 0%,
+		rgba(253, 29, 29, 1) 50%,
+		rgba(252, 176, 69, 1) 100%
+	);
+	background-clip: text;
+	-webkit-background-clip: text;
+	-webkit-text-fill-color: transparent;
 
-  @media (max-width: 768px) {
-    font-size: 3rem;
-  }
+	@media (max-width: 768px) {
+		font-size: 3rem;
+	}
 `;
 
 export const NameSpan = styled.span`
-  font-size: 1.4rem;
-  font-weight: 700;
-  color: #fff;
+	font-size: 1.4rem;
+	font-weight: 700;
+	color: #fff;
 `;
 
 export const DescriptionLocal = styled.p`
-  font-size: 1.2rem;
-  font-weight: 400;
-  color: #fff;
-  width: 500px;
-  text-align: center;
-  margin-bottom: 20px;
-  border: 1px solid #fff;
-  border-radius: 5px;
-  padding: 10px;
-  background-color: rgba(255, 255, 255, 0.2);
+	font-size: 1.2rem;
+	font-weight: 400;
+	color: #fff;
+	width: 500px;
+	text-align: center;
+	margin-bottom: 20px;
+	border: 1px solid #fff;
+	border-radius: 5px;
+	padding: 10px;
+	background-color: rgba(255, 255, 255, 0.2);
 `;
