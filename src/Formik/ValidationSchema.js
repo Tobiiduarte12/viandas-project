be5b24ef89e15426ad7cf434ddb3c06bc800357e
Tobias Duarte <tobiiduarte95@gmail.com>
2023-12-11 20:25:01 -0300

    cambio de api

--- src/Formik/ValidationSchema.js
+++ src/Formik/ValidationSchema.js
@@ -1,25 +1,23 @@
 import * as Yup from "yup";
 
 export const registerValidationSchema = Yup.object({
-	name: Yup.string().trim().required("Completa este campo por favor"),
-	email: Yup.string()
-		.trim()
-		.email("el email debe ser de tipo example@gmail.com")
-		.required("Completa este campo por favor"),
-	password: Yup.string()
-		.trim()
-		.min(6, "Requiere un mínimo de 6 caracteres")
-		.max(10, "Requiere un máximo 10 caracteres")
-		.required("Completa este campo por favor"),
+  name: Yup.string().trim().required("Completa este campo por favor"),
+  email: Yup.string()
+    .trim()
+    .email("el email debe ser de tipo example@gmail.com")
+    .required("Completa este campo por favor"),
+  password: Yup.string()
+    .trim()
+    .min(6, "Requiere un mínimo de 6 caracteres")
+    .required("Completa este campo por favor"),
 });
 
 export const loginValidationSchema = Yup.object({
-	email: Yup.string()
-		.email("el email debe ser de tipo example@gmail.com")
-		.required("Completa este campo por favor"),
-	password: Yup.string()
-		.trim()
-		.min(6, "Requiere un mínimo de 6 caracteres")
-		.max(10, "Requiere un máximo 10 caracteres")
-		.required("Completa este campo por favor"),
+  email: Yup.string()
+    .email("el email debe ser de tipo example@gmail.com")
+    .required("Completa este campo por favor"),
+  password: Yup.string()
+    .trim()
+    .min(6, "Requiere un mínimo de 6 caracteres")
+    .required("Completa este campo por favor"),
 });
