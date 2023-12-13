0319ed02b3286b23e5b780ef04efd2a68bb7874d
Tobias Duarte <tobiiduarte95@gmail.com>
2023-12-13 18:53:52 -0300

    resolved errors

--- src/axios/axios-users.js
+++ src/axios/axios-users.js
@@ -2,26 +2,28 @@ import { BASE_URL } from "../utils/consts";
 import axios from "axios";
 
 export const createUser = async (nombre, email, password) => {
-	try {
-		const response = await axios.post(`${BASE_URL}auth/register`, {
-			nombre,
-			email,
-			password,
-		});
-		return response.data;
-	} catch (error) {
-		return alert(error.response.data.errors[0].message);
-	}
+  try {
+    const response = await axios.post(`${BASE_URL}auth/register`, {
+      nombre,
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return alert(error.response.data.errors[0].message);
+  }
 };
 
 export const loginUser = async (email, password) => {
-	try {
-		const response = await axios.post(`${BASE_URL}auth/login`, {
-			email,
-			password,
-		});
-		return response.data;
-	} catch (error) {
-		return alert(error.response.data.errors[0].message);
-	}
+  try {
+    const response = await axios.post(`${BASE_URL}auth/login`, {
+      email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    console.log(error);
+    return alert(error.response.data.errors[0].message);
+  }
 };
